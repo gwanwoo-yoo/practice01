790c1e7ba4c250e5bd3884b070bca4d26c9ddef3
유관우 <skymill2000@gmail.com>
2022-11-15 17:55:05 +0900

    Homework(사용자): 과제 : 사용자 정보 변경 기능 작성하기

--- src/main/java/com/example/practice01/user/UserController.java
+++ src/main/java/com/example/practice01/user/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,4 +57,12 @@ public class UserController {
     return employeeNumber;
   }
 
+  @PutMapping("/user/{employeeNumber}")
+  public Integer updateUser(
+      @PathVariable Integer employeeNumber,
+      @RequestBody UserFromDatabase userFromDatabase
+  ){
+    //Todo : 사용자 정보 변경 기능 구현
+    return employeeNumber;
+  }
 }
