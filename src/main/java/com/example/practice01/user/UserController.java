c8f095517ae6fdca459268d71284dd2a9cf5b18e
유관우 <skymill2000@gmail.com>
2022-11-15 17:21:54 +0900

    Homework(사용자): 과제 : 사용자 삭제 기능 구현  - employeeId 를 바탕으로 사용자를 삭제 할 수 있는 기능을 구현해 주세요

--- src/main/java/com/example/practice01/user/UserController.java
+++ src/main/java/com/example/practice01/user/UserController.java
@@ -3,7 +3,9 @@ package com.example.practice01.user;
 import com.example.practice01.user.model.User;
 import com.example.practice01.user.model.UserFromDatabase;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,6 +46,14 @@ public class UserController {
     @RequestBody UserFromDatabase userFromDatabase
   ){
     return userService.insertUser(userFromDatabase);
+  }
+
+  @DeleteMapping("/user/{employeeNumber}")
+  public Integer deleteUser(
+      @PathVariable Integer employeeNumber
+  ){
+    //Todo : 사용자 삭제 기능 구현
+    return employeeNumber;
   }
 
 }
